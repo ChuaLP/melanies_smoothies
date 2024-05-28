797d2d760590ffb34f75b0cb687420efe5e3a13b
ChuaLP <49647591+ChuaLP@users.noreply.github.com>
2024-05-28 16:02:11 +0800

    Update streamlit_app.py
    Correction for error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,16 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
-    """
-)
-
-cnx = st.connection("snowflake")
-session.cnx.session()
+    """)
 
 ingredients_string=''
 
 name_on_order=st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session.cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
